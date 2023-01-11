c6f2fcf36acf12b74fa75736bf136cd09307e88a
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-11 23:25:35 +0900

    feat: react app init setting

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginButtonBox from './LoginButtonBox';
+import logo from "./logo.svg";
+import "./App.css";
+import LoginButtonBox from "./LoginButtonBox";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <LoginButtonBox />
       </header>
-      <LoginButtonBox/>
     </div>
   );
 }
--- react-app/src/LoginButtonBox.jsx
+++ react-app/src/LoginButtonBox.jsx
@@ -1,24 +1,55 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+axios.defaults.baseURL = "http://localhost:8080";
 const LoginButtonBox = () => {
-  const [text, setText] = useState("");
-
   useEffect(() => {
     axios({
       url: "/api/hello",
       method: "get",
     }).then((response) => {
-      setText(response.data);
+      //console.log(response);
     });
   }, []);
 
-  return (
-    <>
-    <div>"hello connection test"</div>
-    <div>{text}</div>
+  const postHello = () => {
+    axios
+      .post(
+        "/api/hello",
+        {},
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
+      .then((r) => console.log(r.request.response.url));
+  };
 
-    </>
+  const getHelloRed = () => {
+    axios
+      .get(
+        "/api/hello/red",
+        {},
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
+      .then((r) => console.log(r.request.response.url));
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={postHello}>
+        안녕
+      </button>
+      <a href="http://localhost:8080/api/hello/red">hello redirect </a>
+      <button type="button" onClick={getHelloRed}>
+        안녕 red
+      </button>
+    </div>
   );
 };
 
